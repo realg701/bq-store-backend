0349c53f54994d3289f9a208764b6084fbb8e63d
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2023-08-29 12:27:47 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-export const connectDb = mongoose
+export default function connectDb (){ mongoose
   .connect(process.env.mongoURI)
-  .then(() => console.log("Connected!"));
+  .then(() => console.log("Connected!"));}
