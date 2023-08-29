754cbe5c236363596ed7413a550251a3160f15a8
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2023-08-29 12:29:17 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import connectDb from "./db.js";
 import { userRouter } from "./routes/users.js";
 import { productRouter } from "./routes/products.js";
 import { orderRouter } from "./routes/orders.js";
@@ -11,18 +11,23 @@ import "dotenv/config";
 
 // APIs
 const app = express();
-mongoose.connect(process.env.mongoURI).then(() => console.log("Connected!"));
+connectDb();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// API Variables
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use("/uploads", express.static(__dirname + "/uploads"));
+const PORT = 9999;
+
 // Routes
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use("/users", userRouter);
 app.use("/orders", orderRouter);
 app.use("/products", productRouter);
 app.use("/upload", uploadRouter);
-const PORT = 9999;
+
 // Server
 app.listen(PORT, () => console.log("Server is Running " + PORT));
