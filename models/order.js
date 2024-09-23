f49d2d053b9e4576b140cd4ddac6e2028b14b241
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-24 01:14:54 +0500

    add: order schema

--- models/order.js
+++ models/order.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
-const orderSchema = mongoose.Schema({
-  products: [],
-  name: String,
-  phoneNumber: Number,
-  address: String,
-  email: String,
-});
+const orderSchema = mongoose.Schema(
+  {
+    products: [],
+    name: String,
+    phoneNumber: Number,
+    address: String,
+    email: String,
+  },
+  { timestamps: true }
+);
 
 export const Order = mongoose.model("Order", orderSchema);
--- routes/orders.js
+++ routes/orders.js
@@ -2,3 +2,44 @@ import express from "express";
 import { Order } from "../models/order.js";
 
 export const orderRouter = express.Router();
+
+// // Order API
+// userRouter.post("/order", async (req, res) => {
+//   const { products, name, phoneNumber, address, email } = req.body;
+
+//   const productsExist = await Order.findOne({ email });
+//   if (products == productsExist.products) {
+//     res.send({ message: "product already exist" });
+//     return;
+//   }
+//   const orderData = { products, name, phoneNumber, address, email };
+//   console.log(orderData);
+//   const orderInstance = new Order(orderData);
+//   const savedOrder = await orderInstance.save();
+//   res.send({ order: savedOrder, message: "Ordered sucessfully" });
+// });
+
+// // Login API
+// userRouter.post("/login", async (req, res) => {
+//   const { userName, passWord } = req.body;
+
+//   const isUser = await User.findOne({ userName });
+//   if (isUser) {
+//     const matchPassWord = await bcrypt.compare(passWord, isUser.passWord);
+//     if (matchPassWord) {
+//       const token = jwt.sign({ _id: isUser._id }, process.env.jwt_secret, {
+//         expiresIn: "30d",
+//       });
+//       res.send({
+//         matchPassWord,
+//         message: "Login Successfully",
+//         user: isUser,
+//         token,
+//       });
+//     } else {
+//       res.send({ matchPassWord, message: "Incorrect Password" });
+//     }
+//   } else {
+//     res.send({ message: "Incorrect Username" });
+//   }
+// });
