a37bab1ec9161c01d63f6667bf1dea76f575e0ee
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-25 05:39:07 +0500

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -5,17 +5,23 @@ export const orderRouter = express.Router();
 
 // Order Post API
 orderRouter.post("/order", async (req, res) => {
-  const { products, name, phoneNumber, address, email } = req.body;
+  const { name, phoneNumber, address, email, products } = req.body;
   console.log("Body:", req.body);
   try {
+    if (products.length === 0)
+      return res.json({
+        message: "cart is empty",
+        status: "empty",
+      });
+    console.log("Products:", products);
     const order = await Order.create({
-      products: products,
       name: name,
       phoneNumber: phoneNumber,
       address: address,
       email: email,
+      products: products,
     });
-    console.log("order", order);
+    console.log("Order:", order);
     return res.json({
       message: "order created",
       status: "success",
@@ -30,10 +36,18 @@ orderRouter.post("/order", async (req, res) => {
 
 // Orders Get API
 orderRouter.get("/", async (req, res) => {
-  const allOrders = await Order.find({});
-  if (allOrders.length) {
-    res.send({ orders: allOrders });
-  } else {
-    res.send({ message: "No orders Availabe" });
+  try {
+    const allOrders = await Order.find({});
+    if (allOrders.length) {
+      res.send({ orders: allOrders });
+    } else {
+      res.send({ message: "No orders Availabe" });
+    }
+  } catch (error) {
+    console.log("Error:", error);
+    return res.json({
+      message: error.message,
+      status: "error",
+    });
   }
 });
