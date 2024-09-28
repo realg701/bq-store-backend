d465e526dfbd3385e256d5812e80a6e581e741bb
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-09-28 07:55:21 +0500

    Update product.js

--- models/product.js
+++ models/product.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 
 const productSchema = mongoose.Schema(
   {
-    image: String,
-    title: String,
-    category: String,
-    seller: String,
+    image: { type: String, required: true },
+    title: { type: String, required: true },
+    category: { type: String, required: true },
+    seller: { type: String, required: true },
     price: Number,
     description: String,
+    quantity: Number,
   },
   { timestamps: true }
 );
